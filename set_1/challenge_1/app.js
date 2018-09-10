a45514987813a3eafff201a4b309f391c2e89336
Unknown <casvantwout@gmail.com>
2018-09-10 16:29:56 +0200

    challenge2 fixed

--- set_1/challenge_1/app.js
+++ set_1/challenge_1/app.js
@@ -9,10 +9,15 @@ function hexToString (hex) {
     return string;
 }
 
+// convert string to hex
 let hex = hexToString('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
 
+//log it to make sure it's ok
 console.log(hex);
 
+//convert hex to base64
 let base64String = btoa(hex);
 
+//log it to check if output is correct
 console.log(base64String);
+
---
+++ set_1/challenge_2/app.js
@@ -0,0 +1,22 @@
+//The buffers module provides a way of handling streams of binary data.
+let hex1 = new Buffer('1c0111001f010100061a024b53535009181c', 'hex');
+let hex2 = new Buffer('686974207468652062756c6c277320657965', 'hex');
+
+//make an empty array
+let results = [];
+
+//loop through the first array
+for (let i = 0; i < hex1.length; i++) {
+
+    // add xor of each first element to the results array
+    results.push(hex1[i] ^ hex2[i])
+}
+// make a new buffer and translatre is back to a hexString
+let resultsToHex = new Buffer(results).toString('hex');
+
+console.log(resultsToHex);
+
+
+
+
+
