a9377b25e31d05a00e48c62e4be11229901649cb
Unknown <casvantwout@gmail.com>
2018-09-10 12:09:12 +0200

    added btoa lib

---
+++ set_1/challenge_1/app.js
@@ -0,0 +1,17 @@
+var btoa = require('btoa');
+
+
+function toHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+let hex = toHex('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
+
+let result = btoa(hex);
+console.log(toHex('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'));
+
+console.log(result);
